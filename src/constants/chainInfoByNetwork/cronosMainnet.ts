6be82cac036c176c453456bec421fbadac5cf558
mintednetwork <dev@minted.network>
2024-02-29 15:51:53 +0800

    feat: update the url of cronos explorer

--- src/constants/chainInfoByNetwork/cronosMainnet.ts
+++ src/constants/chainInfoByNetwork/cronosMainnet.ts
@@ -4,7 +4,7 @@ import { addressesByNetwork } from '../addressesByNetwork';
 export const cronosMainnet: ChainInfo = {
   chainId: SupportedChainId.CRONOS_MAINNET,
   label: 'Cronos',
-  explorer: 'https://cronoscan.com',
+  explorer: 'https://explorer.cronos.org',
   scanText: 'Cronoscan',
   rpcUrl: 'https://mainnet.cronoslabs.org/v1/ea0f1c40f912a166bf1224fedd07b442',
   wrappedCurrency: {
